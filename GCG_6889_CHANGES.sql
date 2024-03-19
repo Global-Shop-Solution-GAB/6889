ce822bbc0b5ea7479998ad2c12980d693ac9d08f
Service Web <sw@gssmail.com>
2024-03-19 15:19:37 -0500

    Commit changes in C:\Websites\Git\emarlow\6889\GCG_6889_CHANGES.sql

---
+++ GCG_6889_CHANGES.sql
@@ -0,0 +1,15 @@
+CREATE TABLE "GCG_6889_CHANGES" (
+ "PackingList" CHAR(7),
+ "SO" CHAR(7),
+ "SOSuffix" CHAR(4),
+ "SOLine" CHAR(4),
+ "Part" CHAR(20),
+ "Location" CHAR(2),
+ "Lot" CHAR(15),
+ "Bin" CHAR(6),
+ "NewBin" CHAR(6),
+ "Heat" CHAR(15),
+ "SerialNumber" CHAR(30),
+ "DateTimeChanged" DATETIME,
+ "UserChanged" CHAR(8),
+ "ProgramChanged" CHAR(8) );
