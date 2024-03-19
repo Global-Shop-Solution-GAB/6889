016feb266a33f373828f3efdc914a5613473c6cf
Service Web <sw@gssmail.com>
2024-03-19 15:19:37 -0500

    Commit changes in C:\Websites\Git\emarlow\6889\GCG_6889_SHIPID.sql

---
+++ GCG_6889_SHIPID.sql
@@ -0,0 +1,15 @@
+CREATE TABLE "GCG_6889_SHIPID" (
+ "PackingList" CHAR(7),
+ "SO" CHAR(7),
+ "SOSuffix" CHAR(4),
+ "SOLine" CHAR(4),
+ "Part" CHAR(20),
+ "Location" CHAR(2),
+ "Lot" CHAR(15),
+ "Heat" CHAR(15),
+ "SerialNumber" CHAR(30),
+ "ShipToID" CHAR(6),
+ "NewShipToID" CHAR(6),
+ "DateTimeChanged" DATETIME,
+ "UserChanged" CHAR(8),
+ "ProgramChanged" CHAR(8) );
